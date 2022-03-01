500ff944710ecdc5dce10d0417a695e2da4db358
Chris Cruz <chriscruzz94@gmail.com>
2022-03-01 15:39:27 -0500

    Added products component and updated routes page

---
+++ client/components/AllProducts.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchProducts } from '../store/productsReducer';
+import { Link } from 'react-router-dom';
+
+const AllProducts = () => {
+  //this grabs the robots from the redux store
+  const products = useSelector((state) => state.products);
+  //This gives up the dispatch function from redux
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
+  return (
+    <div>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            <h1>{product.name}</h1>
+            <Link to={`/products/${product.id}`}>
+              <img
+                src={product.imageUrl}
+                style={{ width: '200px', height: '200px' }}
+              />
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AllProducts;
