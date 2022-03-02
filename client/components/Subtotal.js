37a72e098522855c92007f80504e5f1779228b7e
Kathia Villavicencio <kathia.villavicencio1@outlook.com>
2022-03-02 13:56:52 -0600

    Adds Checkout CheckPoroducts cartReducer StateProvider Subtotal

---
+++ client/components/Subtotal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "../store/cartReducer";
+import { useStateValue } from "./StateProvider";
+
+function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
+            </p>
+            {/* <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small> */}
+          </>
+          //Hi
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+      <button>Proceed to Checkout</button>
+    </div>
+  );
+}
+
+export default Subtotal;
