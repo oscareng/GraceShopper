60b9249cb316379c7a76ca978165a1a65e87cbf5
oscareng <orolf001@ucr.edu>
2022-03-02 14:33:00 -0800

    Modified Sequelize Models and Association With Seed File

---
+++ server/db/models/LineItem.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+const LineItem = db.define("LineItem", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: { notEmpty: true },
+  },
+  saleprice: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 20 },
+  size: { type: Sequelize.STRING, allowNull: false, defaultValue: "Medium" },
+  quantity: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 1 },
+});
+
+module.exports = LineItem;
