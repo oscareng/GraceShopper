fcbf665182bbd2fa1213b4acd68a0f83ad705bb1
Adam Sailsman <adamjaysailsman@gmail.com>
2022-03-08 17:18:28 -0500

    Added User Form Page. This page is accessible after user clciks on Checkout button from the Checkout page

--- client/Routes.js
+++ client/Routes.js
@@ -1,20 +1,21 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import { Login, Signup } from "./components/AuthForm";
-import AllProducts from "./components/AllProducts";
-import SingleProduct from "./components/SingleProduct";
-import Confirmation from "./components/Confirmation";
-import { LandingPage } from "./components/LandingPage";
-import Home from "./components/Home";
-import { me } from "./store";
-import WomenProducts from "./components/WomenProducts";
-import MenProducts from "./components/MenProducts";
-import Checkout from "./components/Checkout";
-import Administrator from "./components/Administrator";
-import EditProduct from "./components/EditProduct";
-import SingleProductGuest from "./components/SingleProductGuest";
-import CheckoutGuest from "./components/CheckoutGuest";
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Login, Signup } from './components/AuthForm';
+import AllProducts from './components/AllProducts';
+import SingleProduct from './components/SingleProduct';
+import Confirmation from './components/Confirmation';
+import { LandingPage } from './components/LandingPage';
+import Home from './components/Home';
+import { me } from './store';
+import WomenProducts from './components/WomenProducts';
+import MenProducts from './components/MenProducts';
+import Checkout from './components/Checkout';
+import Administrator from './components/Administrator';
+import EditProduct from './components/EditProduct';
+import SingleProductGuest from './components/SingleProductGuest';
+import CheckoutGuest from './components/CheckoutGuest';
+import UserForm from './components/UserForm';
 
 /**
  * COMPONENT
@@ -41,6 +42,7 @@ class Routes extends Component {
         <Route exact path="/mens" component={MenProducts} />
         <Route exact path="/administrator" component={Administrator} />
         <Route exact path="/editproduct/:id" component={EditProduct} />
+        <Route exact path="/form" component={UserForm} />
       </div>
     );
   }
---
+++ client/components/UserForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { makeStyles, Typography } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '300px',
+    },
+    '& .MuiButtonBase-root': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
+
+const Form = () => {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root}>
+      <Typography variant="h4"> Customer Information </Typography>
+      <TextField label="First Name" variant="filled" required />
+      <TextField label="Last Name" variant="filled" required />
+      <TextField label="Email" variant="filled" type="email" required />
+      <TextField label="Address 1" variant="filled" required />
+      <TextField label="Address 2" variant="filled" required />
+      <TextField
+        label="City, State, Zip"
+        variant="filled"
+        type="password"
+        required
+      />
+      <div>
+        <Link to="/confirmation" style={{ textDecoration: 'none' }}>
+          <Button type="submit" variant="contained" color="primary">
+            Confirm Order
+          </Button>
+        </Link>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
