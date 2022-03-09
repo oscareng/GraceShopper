dfd02b7c264dc0fe4422569605b9a87d6cac0c7f
Adam Sailsman <adamjaysailsman@gmail.com>
2022-03-09 08:50:25 -0500

    testing outside of main branch to see if I can fix heroku deploy

--- client/components/EditProduct.js
+++ client/components/EditProduct.js
@@ -1,75 +1,61 @@
-import React from "react";
-import { deleteProduct, updateProduct } from "../store/administratorReducer";
+import React from 'react';
+import { deleteProduct, updateProduct } from '../store/administratorReducer';
 import {
   fetchSingleProduct,
   getSingleProduct,
-} from "../store/singleProductReducer";
-import { connect } from "react-redux";
-import products from "../store/productsReducer";
-
+} from '../store/singleProductReducer';
+import { connect } from 'react-redux';
 export class EditProduct extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
+      name: '',
       price: 0,
-      gender: "",
-      size: "",
-      description: "",
-      category: "",
-      stock: "",
-      imageUrl: "",
+      gender: '',
+      size: '',
+      description: '',
+      category: '',
+      stock: '',
+      imageUrl: '',
     };
-
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchSingleProduct(id);
   }
-
   componentWillUnmount() {
     this.props.clearProduct();
   }
-
   componentDidUpdate(prevProps) {
     if (prevProps.product.id !== this.props.product.id) {
       this.setState({
-        name: this.props.product.name || "",
+        name: this.props.product.name || '',
         price: this.props.product.price || 0,
-        gender: this.props.product.gender || "",
-        size: this.props.product.size || "",
-        description: this.props.product.description || "",
-        category: this.props.product.category || "",
-        stock: this.props.product.stock || "",
-        imageUrl: this.props.product.imageUrl || "",
+        gender: this.props.product.gender || '',
+        size: this.props.product.size || '',
+        description: this.props.product.description || '',
+        category: this.props.product.category || '',
+        stock: this.props.product.stock || '',
+        imageUrl: this.props.product.imageUrl || '',
       });
     }
   }
-
   handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value,
     });
   }
-  // handlePriceChange(evt) {
-  //   const newPrice = parseInt(evt.target.value);
-  //   this.setState({
-  //     [evt.target.name]: newPrice,
-  //   });
-  // }
   handleSubmit(evt) {
     evt.preventDefault();
-    console.log("mystate", this.state);
-    console.log("myproduct", this.props.product);
+    console.log('mystate', this.state);
+    console.log('myproduct', this.props.product);
     this.props.updateProduct({
       ...this.props.product,
       ...this.state,
     });
   }
-
   render() {
     const {
       name,
@@ -81,42 +67,33 @@ export class EditProduct extends React.Component {
       stock,
       imageUrl,
     } = this.state;
-
     const { handleSubmit, handleChange } = this;
-
     return (
       <div>
         <form id="product-form" onSubmit={handleSubmit}>
           <label htmlFor="name">Name:</label>
           <input name="name" onChange={handleChange} value={name} />
-
           <label htmlFor="price">Price:</label>
           <input name="price" onChange={handleChange} value={price} />
-
           <label htmlFor="gender">Gender:</label>
           <select name="gender" onChange={handleChange} value={gender}>
             <option value="Female">Female</option>
             <option value="Male">Male</option>
           </select>
-
           {/* <input name="gender" onChange={handleChange} value={gender} /> */}
-
           <label htmlFor="size">Size:</label>
           <select name="size" value={size} onChange={handleChange}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
           </select>
-
           {/* <input name="size" onChange={handleChange} value={size} /> */}
-
           <label htmlFor="description">Description:</label>
           <input
             name="description"
             onChange={handleChange}
             value={description}
           />
-
           <label htmlFor="category">Category:</label>
           {/* <select name="category" value={category} onChange={handleChange}>
             <option value="Shirt">Shirt</option>
@@ -125,12 +102,9 @@ export class EditProduct extends React.Component {
             <option value="Blazer">Blazer</option>
             <option value="Cap">Cap</option>
           </select> */}
-
           <input name="category" onChange={handleChange} value={category} />
-
           <label htmlFor="stock">Stock:</label>
           <input name="stock" onChange={handleChange} value={stock} />
-
           <label htmlFor="imageUrl">Image:</label>
           {/* <input
             type="file"
@@ -141,12 +115,10 @@ export class EditProduct extends React.Component {
             value={imageUrl}
           /> */}
           <input name="imageUrl" onChange={handleChange} value={imageUrl} />
-
           <div>
             <button type="submit">Update product</button>
           </div>
         </form>
-
         <form onSubmit={(ev) => ev.preventDefault()}>
           <button
             className="delete"
@@ -159,16 +131,13 @@ export class EditProduct extends React.Component {
     );
   }
 }
-
 const mapStateToProps = ({ product }) => ({
   product,
 });
-
 const mapDispatchToProps = (dispatch, { history }) => ({
   updateProduct: (product) => dispatch(updateProduct(product, history)),
   deleteProduct: (product) => dispatch(deleteProduct(product, history)),
   fetchSingleProduct: (id) => dispatch(fetchSingleProduct(id)),
   clearProduct: () => dispatch(getSingleProduct({})),
 });
-
 export default connect(mapStateToProps, mapDispatchToProps)(EditProduct);
